5f66e2d233dd03371099f4626914a84795ae2b1d
masao <Masao-Someki>
2020-06-03 21:55:09 +0900

    Ennable to set fs/shiftms from run.sh
    - modified decode/feature-extract related script

--- run.sh
+++ run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# stage settings
 stage=r123
 
+# speakers to train
 all_spks=(jvs001 jvs002)
 
+# directories
 wav_dir=wav
 figure_dir=figure
 data_dir=data
 model_dir=model
 exp_dir=exp
+
+# wav and extract info
+fs=24000
+shiftms=5
+mcep_dim=49
+fftl=1024
+
+# number of cpus
 n_jobs=16
 
 . parse_options.sh
@@ -40,9 +51,9 @@ if echo ${stage} | grep -q 0; then
 
 		# process
 		python src/prepro/create_hist.py \
-						--n_jobs ${n_jobs} \
-						--wav_dir ${wav_dir}/train/${spk} \
-						--figure_dir ${figure_dir}/${spk}
+			--n_jobs ${n_jobs} \
+			--wav_dir ${wav_dir}/train/${spk} \
+			--figure_dir ${figure_dir}/${spk}
 	done
 fi
 
@@ -62,19 +73,29 @@ if echo ${stage} | grep -q 1; then
 
 		# training data
 		python src/prepro/extract_feature.py \
-						--log_dir ${exp_dir}/extract \
-						--wav_dir ${wav_dir}/train/${spk} \
-						--hdf5dir ${data_dir}/train/${spk} \
-						--conf_path ./config/speaker/${spk}.conf \
-						--n_jobs ${n_jobs}
+			--log_dir ${exp_dir}/extract \
+			--wav_dir ${wav_dir}/train/${spk} \
+			--hdf5dir ${data_dir}/train/${spk} \
+			--conf_path ./config/speaker/${spk}.conf \
+			--fs ${fs} \
+			--shiftms ${shiftms} \
+			--mcep_dim ${mcep_dim} \
+			--fftl ${fftl} \
+			--n_jobs ${n_jobs}
 
 		# val data
 		python src/prepro/extract_feature.py \
-						--log_dir ${exp_dir}/extract \
-      			--wav_dir ${wav_dir}/val/${spk} \
-      			--hdf5dir ${data_dir}/val/${spk} \
-      			--conf_path ./config/speaker/${spk}.conf \
-      			--n_jobs ${n_jobs}
+			--log_dir ${exp_dir}/extract \
+			--wav_dir ${wav_dir}/val/${spk} \
+			--hdf5dir ${data_dir}/val/${spk} \
+			--conf_path ./config/speaker/${spk}.conf \
+			--fs ${fs} \
+			--shiftms ${shiftms} \
+			--fs ${fs} \
+			--shiftms ${shiftms} \
+			--mcep_dim ${mcep_dim} \
+			--fftl ${fftl} \
+			--n_jobs ${n_jobs}
 	done
 	# calc total stats
   python src/prepro/calc_stats.py \
@@ -94,9 +115,9 @@ if echo ${stage} | grep -q 2; then
 	        --total_stats ${data_dir}/total_stats.h5 \
 	        --conf_path ./config/vc.conf \
 	        --model_dir ${model_dir} \
-		--model_name test \
-	        --decode_dir ${exp_dir}/test \
-	        --log_name test
+		--model_name branch-test \
+	        --decode_dir ${exp_dir}/branch-test \
+	        --log_name branch-test
 	       	#--resume ${model_dir}/jvs001-yukari.5507.pt
 fi
 
@@ -104,11 +125,17 @@ fi
 ############ stage 3 ################3
 # decode
 if echo ${stage} | grep -q 3; then
+	decode_dir=${exp_dir}/branch-test
+	mkdir -p ${decode_dir}
 	python src/decode.py \
 		--test_dir ${data_dir}/val \
-		--exp_dir ${exp_dir}/test \
+		--exp_dir ${decode_dir} \
 		--stats_dir ${data_dir}/stats \
 		--conf_path ./config/vc.conf \
-		--checkpoint ${model_dir}/jvs001-jvs002.101360.pt \
-		--log_name decode_vae
+		--checkpoint ${model_dir}/branch-test.1448.pt \
+		--log_name branch-test \
+		--fs ${fs} \
+		--shiftms ${shiftms} \
+		--mcep_dim ${mcep_dim} \
+		--fftl ${fftl}
 fi
